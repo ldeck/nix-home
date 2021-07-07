67f3fa58daa69993d80ae0f4d6b989e367836f63
Lachlan Deck <lachlan.deck@gmail.com>
2021-07-07 23:59:37 +1000

    home.nix: load git personalisation from ~/.me.d/git.nix
    Example
    {
      email = "foo.bar@example.com";
      name = "Foo Bar";
    }

--- home.nix
+++ home.nix
@@ -1,5 +1,35 @@
 { pkgs, ... }:
 
+let
+
+  # ---------------------------------------------------------
+  # FUNCTIONS
+  # ---------------------------------------------------------
+
+  tryGetAttr = key: set: msg:
+     if builtins.hasAttr key set
+     then builtins.getAttr key set
+     else throw msg;
+
+  tryImport = path:
+    if builtins.pathExists path
+    then import path
+    else throw "${path} does not exist";
+
+  # ---------------------------------------------------------
+  # VARIABLES
+  # ---------------------------------------------------------
+
+  homedir = builtins.getEnv "HOME";
+  username = builtins.getEnv "USER";
+
+  __gitpath = "${homedir}/.me.d/git.nix";
+  __gitinfo = tryImport __gitpath;
+
+  fullname = tryGetAttr "name" __gitinfo ''${__gitpath} missing attribute 'name'. e.g., name = "Lachlan Deck";'';
+  email = tryGetAttr "email" __gitinfo "${__gitpath} missing attribute 'email'.";
+
+in
 {
 
   # The home-manager manual is at:
@@ -15,8 +45,8 @@
   #
   # You need to change these to match your username and home directory
   # path:
-  home.username = "ldeck";
-  home.homeDirectory = "/Users/ldeck";
+  home.username = username;
+  home.homeDirectory = homedir;
 
   # If you use non-standard XDG locations, set these options to the
   # appropriate paths:
@@ -35,12 +65,16 @@
   # changes in each release.
   home.stateVersion = "20.09";
 
+  # ----------------------------------------------------------------
+  # programs
+  # ----------------------------------------------------------------
+  programs.autojump.enable = true;
+
   # Since we do not install home-manager, you need to let home-manager
   # manage your shell, otherwise it will not be able to add its hooks
   # to your profile.
-  programs.bash = {
-    enable = true;
-  };
+  programs.bash.enable = true;
+  programs.direnv.enable = true;
 
   programs.emacs = {
     enable = true;
@@ -52,12 +86,35 @@
 
   programs.git = {
     enable = true;
-    userName = "Lachlan Deck";
-    userEmail = "lachlan.deck@gmail.com";
+    userName = fullname;
+    userEmail = email;
     aliases = {
-      st = "status";
+      unstage = "reset HEAD --";
+      pr = "pull --rebase";
+      co = "checkout";
+      ci = "commit";
+      c = "commit";
+      b = "branch";
+      p = "push";
+      d = "diff";
+      a = "add";
+      s = "status";
+      f = "fetch";
+      br = "branch";
+      lr = "reflog";
+      l = "log --graph --pretty='%Cred%h%Creset - %C(bold blue)<%an>%Creset %s%C(yellow)%d%Creset %Cgreen(%cr)' --abbrev-commit --date=relative";
+      hist = "log --branches --remotes --decorate --graph --pretty=format:'%C(auto) %>|(15) %h %d %<|(30trunc) %s %>|(100)%C(cyan)%ci %C(yellow)%an %C(cyan)(%ar)'";
+      h = "hist";
+    };
+    ignores = [ "*~" "__pycache__" "*.pyc" "*.swp" ".*-cache" "build/" ];
+    extraConfig = {
+      merge.conflictstyle = "diff3";
+      pull.rebase = true;
+      core.quotepath = true;
     };
   };
+
+  programs.home-manager.enable = true;
 
   programs.zsh = {
     enable = true;
