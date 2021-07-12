7c4a0736eba01df0308e2ae74245e577d4cafc9a
Lachlan Deck <lachlan.deck@gmail.com>
2021-07-12 11:08:37 +1000

    direnv: configure stdlib with custom use_java function
    Currently enables using jdk8_headless, jdk11_headless, jdk16_headless (default).

---
+++ lib/defaults/direnv.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }:
+
+let
+
+  java_home = pkgs.writeShellScriptBin "java_home" ''
+    if [ "$#" -ne 2 ] || [ "$1" != "-v" ] || [ "$2" -lt 8 ]; then
+        echo "Usage: java_home -v <version>";
+        exit 1;
+      fi
+      case "$2" in
+        8)
+          JDK="${pkgs.jdk8_headless}"
+          ;;
+        11)
+          JDK="${pkgs.jdk11}"
+          ;;
+        *)
+          JDK="${pkgs.jdk16}"
+          ;;
+      esac
+      JAVA_HOME=$(${pkgs.coreutils}/bin/realpath "$JDK/bin/..")
+      echo "$JAVA_HOME"
+  '';
+
+in
+{
+  programs.direnv = {
+    enable = true;
+    stdlib = ''
+      use_java() {
+        JAVA_HOME=$(${java_home}/bin/java_home -v $1)
+        export JAVA_HOME="$JAVA_HOME"
+        echo "JAVA_HOME=$JAVA_HOME"
+        load_prefix "$JAVA_HOME"
+        PATH_add "$JAVA_HOME/bin"
+      }
+    '';
+  };
+
+  home.packages = [ java_home ];
+}
