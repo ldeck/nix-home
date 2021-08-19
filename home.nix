0ff4c9c5d75286cfd5a0a03351ba23f18efc1e9d
Lachlan Deck <lachlan.deck@gmail.com>
2021-08-20 06:20:52 +1000

    pkgs.config: allowUnfree, permittedInsecurePackages [openssl]

--- home.nix
+++ home.nix
@@ -35,6 +35,13 @@ in
 {
   imports = modules;
 
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = [
+      "openssl-1.0.2u"
+    ];
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
