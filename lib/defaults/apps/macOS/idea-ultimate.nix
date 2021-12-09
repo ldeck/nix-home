3102e10c3421ec6ba6f06fc09badf71a1a3bc031
Lachlan Deck <lachlan.deck@gmail.com>
2021-12-10 07:56:36 +1100

    macapps: update intellij to 2021.3 (add support for aarch64)

--- lib/defaults/apps/macOS/idea-ultimate.nix
+++ lib/defaults/apps/macOS/idea-ultimate.nix
@@ -8,7 +8,22 @@
 with lib;
 
 let
+  stdenv = pkgs.stdenv;
   cfg = config.macOS.apps.idea-ultimate;
+
+  ideaSpecs = {
+    x86_64-darwin = {
+      version = "2021.3";
+      arch = "";
+      sha256 = "463e861c5357bef19f498f4bffa06f7e912d59f7a795eda2c603b02d4d737de0";
+    };
+    aarch64-darwin = {
+      version = "2021.3";
+      arch = "-aarch64";
+      sha256 = "9f9186b9a9ac97c656fb3f40fd0880e0d84957e66e19e72a20d6f15d2cd92b41";
+    };
+  };
+
 in {
   options = {
     macOS.apps.idea-ultimate = {
@@ -17,11 +32,11 @@ in {
         description = "Whether to enable this app.";
       };
       version = mkOption {
-        default = "2021.2.1";
+        default = ideaSpecs.${stdenv.hostPlatform.system}.version;
         description = "The version of the app";
       };
       sha256 = mkOption {
-        default = "1cyidcbd4pzzb83ngdh54xdxna6g3nrp070lp2xmlgjhrlf9p1x1";
+        default = ideaSpecs.${stdenv.hostPlatform.system}.sha256;
         description = "The sha256 for the defined version";
       };
     };
