05ea5db17f5f1d8dcd81c0f257eaa5fca898bd65
Lachlan Deck <lachlan.deck@gmail.com>
2021-12-11 05:06:15 +1100

    mac apps: update Chromium to 948906 (and add aarch64 support)

--- lib/defaults/apps/macOS/chromium.nix
+++ lib/defaults/apps/macOS/chromium.nix
@@ -8,7 +8,22 @@
 with lib;
 
 let
+  stdenv = pkgs.stdenv;
   cfg = config.macOS.apps.chromium;
+
+  specs = {
+    x86_64-darwin = {
+      version = "948906";
+      arch = "Mac";
+      sha256 = "dee4dbb296e672d2e7e552d865d82c3248b2534b232ee638f472aa3a8948b223";
+    };
+    aarch64-darwin = {
+      version = "948906";
+      arch = "Mac_Arm";
+      sha256 = "823c1c56a981619d4a1fb029ba415ab6a0d47caf70c70d337d64651a2e6d82d9";
+    };
+  };
+
 in {
   options = {
     macOS.apps.chromium = {
@@ -16,12 +31,16 @@ in {
         default = false;
         description = "Whether to enable this app.";
       };
+      arch = mkOption {
+        default = specs.${stdenv.hostPlatform.system}.arch;
+        description = "The chromium alias for the host platform's system. e.g., Mac, Mac_Arm";
+      };
       version = mkOption {
-        default = "918450";
+        default = specs.${stdenv.hostPlatform.system}.version;
         description = "The version of this app";
       };
       sha256 = mkOption {
-        default = "11bn7finc76kamdrh61icvg35wfnpch3rpxpa0gigzwar3gfn7q2";
+        default = specs.${stdenv.hostPlatform.system}.sha256;
         description = "The sha256 for the defined version";
       };
     };
@@ -34,7 +53,7 @@ in {
         sourceRoot = "chrome-mac/${name}.app";
         version = cfg.version;
         src = pkgs.fetchurl {
-          url = "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/${version}/chrome-mac.zip";
+          url = "https://commondatastorage.googleapis.com/chromium-browser-snapshots/${cfg.arch}/${version}/chrome-mac.zip";
           sha256 = cfg.sha256;
         };
         description = "Chromium is an open-source browser project that aims to build a safer, faster, and more stable way for all Internet users to experience the web.";
