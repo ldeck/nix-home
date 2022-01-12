9fe65e59803473fa3d80209203cb90ebe6c04345
Lachlan Deck <lachlan.deck@gmail.com>
2022-01-12 19:18:52 +1100

    update macOS.apps.firefox to 96.0

--- lib/defaults/apps/macOS/firefox.nix
+++ lib/defaults/apps/macOS/firefox.nix
@@ -4,7 +4,6 @@
   pkgs,
   ...
 }:
-
 with lib;
 
 let
@@ -14,33 +13,33 @@ in {
     macOS.apps.firefox = {
       enable = mkOption {
         default = false;
-        description = "Whether to enable this app.";
+        description = "Whether to enable this cask.";
       };
       version = mkOption {
-        default = "95.0";
-        description = "The version of the app";
+        default = "96.0";
+        description = "The version of the cask.";
       };
       sha256 = mkOption {
-        default = "df8f92bbaa62ad04a70b38d71fb75b5181250fffdb6085f6698fa33cce6c0704";
-        description = "The sha256 for the defined version";
+        default = "8c1b31765c245f23bbcdb84b8ffe0edb93f3dae7dfc561ec3f44c5378ad13019";
+        description = "The sha256 for the cask.";
       };
     };
   };
-
   config = mkIf cfg.enable {
     home.packages =
       (pkgs.callPackage ./lib/app.nix rec {
         name = "Firefox";
-        sourceRoot = "Firefox.app";
+        description = "Web browser";
+        sourceRoot = "${name}.app";
         version = cfg.version;
+        versionedName = "${name}-${version}";
         src = pkgs.fetchurl {
-          url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/${version}/mac/en-US/Firefox+${version}.dmg";
+          url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/${cfg.version}/mac/en-US/Firefox%20${cfg.version}.dmg";
           sha256 = cfg.sha256;
-          name = "Firefox-${version}.dmg";
+          name = "${versionedName}.dmg";
         };
-        description = "The Firefox web browser";
-        homepage = https://www.mozilla.org/en-US/firefox/;
-        appcast = https://www.mozilla.org/en-US/firefox/releases/;
+        appcast = "https://formulae.brew.sh/api/cask/firefox.json";
+        homepage = "https://www.mozilla.org/firefox/";
       });
   };
 }
