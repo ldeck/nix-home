1c407b95dd8379a70d5736a4dfc5e40ea7f51dba
Lachlan Deck <lachlan.deck@gmail.com>
2022-02-25 06:38:50 +1100

    update Signal.app to 5.33.0

--- lib/defaults/apps/macOS/signal.nix
+++ lib/defaults/apps/macOS/signal.nix
@@ -1,10 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-
+{ config, lib, pkgs, ... }:
 with lib;
 
 let
@@ -17,29 +11,29 @@ in {
         description = "Whether to enable this app.";
       };
       version = mkOption {
-        default = "5.25.1";
-        description = "The version of the app";
+        default = "5.33.0";
+        description = "The version of the app.";
       };
       sha256 = mkOption {
-        default = "03351b238d118b853e2856e623c66b25f9f44da068befdae6e40b8f46d17b817";
-        description = "The sha256 for the defined version";
+        default = "5e918f99bc156c25b779386c45990c15c7994d24c65c3be61d002a7b829bcd4d";
+        description = "The sha256 for the app.";
       };
     };
   };
-
   config = mkIf cfg.enable {
     home.packages =
       (pkgs.callPackage ./lib/app.nix rec {
         name = "Signal";
-        sourceRoot = "Signal.app";
+        description = "Instant messaging application focusing on security";
+        sourceRoot = "${name}.app";
         version = cfg.version;
         src = pkgs.fetchurl {
-          url = "https://updates.signal.org/desktop/signal-desktop-mac-${version}.dmg";
+          url = "https://updates.signal.org/desktop/signal-desktop-mac-x64-${cfg.version}.dmg";
           sha256 = cfg.sha256;
+          name = "${name}-${version}.dmg";
         };
-        description = "Cross-platform instant messaging application focusing on security";
+        appcast = "https://formulae.brew.sh/api/cask/signal.json";
         homepage = "https://signal.org/";
-        appcast = "https://github.com/signalapp/Signal-Desktop/releases.atom";
       });
   };
 }
