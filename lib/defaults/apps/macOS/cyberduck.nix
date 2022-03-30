268572d8240f98970c1e7c7d914537c4e02c799e
Lachlan Deck <lachlan.deck@gmail.com>
2022-03-31 09:04:04 +1100

    update Cyberduck to 8.3.0

--- lib/defaults/apps/macOS/cyberduck.nix
+++ lib/defaults/apps/macOS/cyberduck.nix
@@ -3,6 +3,9 @@ with lib;
 
 let
   cfg = config.macOS.apps.cyberduck;
+  arch = if stdenv.isDarwin stdenv.hostPlatform.darwinArch else stdenv.system;
+  toHyphenedLower = str:
+    (lib.strings.toLower (builtins.replaceStrings [" "] ["-"] str));
 in {
   options = {
     macOS.apps.cyberduck = {
@@ -15,15 +18,15 @@ in {
         description = "The app folder name to recursively copy from the install archive. e.g., Foo.app";
       };
       version = mkOption {
-        default = "8.2.3";
+        default = "8.3.0";
         description = "The version of the app.";
       };
       buildNumber = mkOption {
-        default = "36880";
+        default = "37309";
         description = "The build number of the app (if applicable).";
       };
       sha256 = mkOption {
-        default = "edac85691a21fee1c1d10fc72dfc7ca91230273bc31a37d0a7b8b1e8afa30d80";
+        default = "da992c3d1082166d3f780c73cc0dd62fa3450a00c502df954f7e6c3886ef1b5e";
         description = "The sha256 for the app.";
       };
     };
@@ -38,7 +41,7 @@ in {
         src = pkgs.fetchurl {
           url = "https://update.cyberduck.io/Cyberduck-${cfg.version}.${cfg.buildNumber}.zip";
           sha256 = cfg.sha256;
-          name = "${name}-${version}.zip";
+          name = "${(toHyphenedLower name)}-${arch}-${version}.zip";
         };
         appcast = "https://formulae.brew.sh/api/cask/cyberduck.json";
         homepage = "https://cyberduck.io/";
