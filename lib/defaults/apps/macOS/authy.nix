cf944ba24d99a2fe11a4abef4cc8ec2af153ca42
Lachlan Deck <lachlan.deck@gmail.com>
2022-03-31 09:07:06 +1100

    update Authy to 2.1.0

--- lib/defaults/apps/macOS/authy.nix
+++ lib/defaults/apps/macOS/authy.nix
@@ -3,9 +3,9 @@ with lib;
 
 let
   cfg = config.macOS.apps.authy;
-  inputChars = lib.strings.stringToCharacters "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-  outputChars = lib.strings.stringToCharacters "abcdefghijklmnopqrstuvwxyz-";
-  toHyphenedLower = str: builtins.replaceStrings inputChars outputChars str;
+  arch = if stdenv.isDarwin stdenv.hostPlatform.darwinArch else stdenv.system;
+  toHyphenedLower = str:
+    (lib.strings.toLower (builtins.replaceStrings [" "] ["-"] str));
 in {
   options = {
     macOS.apps.authy = {
@@ -13,12 +13,16 @@ in {
         default = false;
         description = "Whether to enable this app.";
       };
+      sourceRoot = mkOption {
+        default = "Authy Desktop.app";
+        description = "The app folder name to recursively copy from the install archive. e.g., Foo.app";
+      };
       version = mkOption {
-        default = "1.9.0";
+        default = "2.1.0";
         description = "The version of the app.";
       };
       sha256 = mkOption {
-        default = "6cb1c94df75f4575148f369bba30ba0c5f49f563370545ecd687658090c0ac08";
+        default = "988f53a6d18fce648f5c6f6055cb7db644f186667a3d7fce6571f288f94e70e6";
         description = "The sha256 for the app.";
       };
     };
@@ -27,14 +31,13 @@ in {
     home.packages =
       (pkgs.callPackage ./lib/app.nix rec {
         name = "Authy";
-        appName = "${name} Desktop";
         description = "Two-factor authentication software";
-        sourceRoot = "${appName}.app";
+        sourceRoot = cfg.sourceRoot;
         version = cfg.version;
         src = pkgs.fetchurl {
           url = "https://authy-electron-repository-production.s3.amazonaws.com/authy/stable/${cfg.version}/darwin/x64/Authy%20Desktop-${cfg.version}.dmg";
           sha256 = cfg.sha256;
-          name = "${toHyphenedLower name}-${version}.dmg";
+          name = "${(toHyphenedLower name)}-${arch}-${version}.dmg";
         };
         appcast = "https://formulae.brew.sh/api/cask/authy.json";
         homepage = "https://authy.com/";
