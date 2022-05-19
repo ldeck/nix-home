3c324e5dc2084fe81cfd26b038c29489bcdd91e4
Lachlan Deck <lachlan.deck@gmail.com>
2022-05-19 15:27:28 +1000

    direnv: split java and ruby config to separate files

--- lib/defaults/java.nix
+++ lib/defaults/java.nix
@@ -61,52 +61,12 @@ in
           '';
         };
       };
-      use_chruby = {
-        enable = mkOption {
-          default = false;
-          description = "Whether to enable hte use of chruby in direnv";
-        };
-        script = mkOption {
-          type = types.str;
-          default = ''
-            use_ruby() {
-              # enable the chruby command in an environment
-              source ${pkgs.chruby}/share/chruby/chruby.sh
-
-              # desired Ruby version as first parameter
-              local ver=$1
-
-              # if version not given as parameter and there is a .ruby-version file, get
-              # version from the file
-              if [[ -z $ver ]] && [[ -f .ruby-version ]]; then
-                ver=$(cat .ruby-version)
-              fi
-
-              # if the version still isn't set, error cause we don't know what to do
-              if [[ -z $ver ]]; then
-                echo Unknown ruby version
-                exit 1
-              fi
-
-              # switch to the desired ruby version
-              chruby $ver
-
-              # Sets the GEM_HOME environment variable to `$PWD/.direnv/ruby/RUBY_VERSION`.
-              # This forces the installation of any gems into the project’s sub-folder. If
-              # you’re using bundler it will create wrapper programs that can be invoked
-              # directly instead of using the `bundle exec` prefix.
-              layout_ruby
-            }
-          '';
-        };
-      };
     };
   };
 
-  config = mkIf (cfg.use_java.enable || cfg.use_chruby.enable) {
+  config = mkIf (cfg.use_java.enable) {
     programs.direnv.stdlib = mkDefault ''
       ${if cfg.use_java.enable then cfg.use_java.script else ""}
-      ${if cfg.use_chruby.enable then cfg.use_chruby.script else ""}
     '';
 
     home.packages = if cfg.use_java.enable then [ java_home ] else [];
--- lib/defaults/ruby.nix
+++ lib/defaults/ruby.nix
@@ -3,83 +3,58 @@
 with lib;
 
 let
-  cfg = config.programs.ruby-install;
-  stdenv = pkgs.stdenv;
-  # can't get it to work on darwin purely yet
-  gcc = if stdenv.isDarwin then "/usr/bin/gcc" else "${pkgs.gcc}/bin/gcc";
+
+  cfg = config.programs.direnv;
+
 in
 {
   options = {
-    programs.ruby-install = {
-      enable = mkOption {
-        default = false;
-        description = "Whether to enable ruby-install.";
-      };
-      version = mkOption {
-        default = "0.8.3";
-        description = "The version of ruby-install.";
-      };
-      sha256 = mkOption {
-        default = "EJISa4q1tATAZ2E5CkBeOMskGG0DF2ewNnhGfGCKrkE=";
-        description = "The sha256 for ruby-install.";
+    programs.direnv = {
+      use_chruby = {
+        enable = mkOption {
+          default = false;
+          description = "Whether to enable the use of chruby in direnv";
+        };
+        script = mkOption {
+          type = types.str;
+          default = ''
+            use_ruby() {
+              # enable the chruby command in an environment
+              source ${pkgs.chruby}/share/chruby/chruby.sh
+
+              # desired Ruby version as first parameter
+              local ver=$1
+
+              # if version not given as parameter and there is a .ruby-version file, get
+              # version from the file
+              if [[ -z $ver ]] && [[ -f .ruby-version ]]; then
+                ver=$(cat .ruby-version)
+              fi
+
+              # if the version still isn't set, error cause we don't know what to do
+              if [[ -z $ver ]]; then
+                echo Unknown ruby version
+                exit 1
+              fi
+
+              # switch to the desired ruby version
+              chruby $ver
+
+              # Sets the GEM_HOME environment variable to `$PWD/.direnv/ruby/RUBY_VERSION`.
+              # This forces the installation of any gems into the project’s sub-folder. If
+              # you’re using bundler it will create wrapper programs that can be invoked
+              # directly instead of using the `bundle exec` prefix.
+              layout_ruby
+            }
+          '';
+        };
       };
     };
   };
-  config = mkIf cfg.enable {
-    home.packages = [
-      (
-        let
-          ruby-install = stdenv.mkDerivation rec {
-            name = "ruby-install-${version}";
-            version = cfg.version;
 
-            src = pkgs.fetchFromGitHub {
-              owner = "postmodern";
-              repo = "ruby-install";
-              rev = "v${version}";
-              sha256 = cfg.sha256;
-            };
-
-            buildInputs = with pkgs; [
-              coreutils
-            ];
-
-            makeFlags = [
-              "PREFIX=$(out)"
-              "SHELL=${pkgs.bash}/bin/bash"
-            ];
-
-            preInstall = ''
-              mkdir -p $out/bin
-              mkdir -p $out/share
-            '';
-          };
-
-          ruby-install-wrapper = pkgs.writeShellScriptBin "ruby-install" ''
-            export CC=${gcc}
-
-            SOURCEDIR=" -s ~/.rubies/src "
-            OPENSSLDIR=" --with-openssl-dir=${pkgs.openssl.dev} "
-            READLINEDIR=" --with-readline-dir=${pkgs.readline.dev} "
-
-            for var in "$@"; do
-              case "$var" in
-                -s) SOURCEDIR="" ;;
-                --with-readline-dir=*) OPENSSLDIR="" ;;
-                --with-readline-dir=*) READLINEDIR="" ;;
-                *) ;;
-              esac
-            done
-
-            SWITCHES="$SOURCEDIR"
-            CONFIGS="$${OPENSSLDIR}$${READLINEDIR}"
-            [ ! -z "$CONFIGS" ] && CONFIGS="-- $CONFIGS"
-            ${ruby-install}/bin/ruby-install $SWITCHES $@ $CONFIGS
-          '';
-
-        in
-          ruby-install-wrapper
-      )
-    ];
+  config = mkIf (cfg.use_chruby.enable) {
+    programs.direnv.stdlib = mkDefault ''
+      ${if cfg.use_chruby.enable then cfg.use_chruby.script else ""}
+    '';
   };
 }
