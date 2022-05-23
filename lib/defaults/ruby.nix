9aeee507180634b886c1c6f3a784abbdc235c732
Lachlan Deck <lachlan.deck@gmail.com>
2022-05-23 18:13:17 +1000

    ruby: configurable packages populated from nixpkgs
    The ruby module will figure out what ruby versions are available in
    the current nixpkgs and use those by default.
    
    The programs.ruby.packages attribute can be used to provide a custom
    set of ruby derivations.

--- lib/defaults/ruby.nix
+++ lib/defaults/ruby.nix
@@ -6,19 +6,14 @@ with attrsets;
 let
   cfg = config.programs.ruby;
 
-  mkRubyLoc = dir: r: "${dir}/ruby-${r.version}";
-  mkRubySrc = dir: r: {
-    source = r;
-    target = mkRubyLoc dir r;
-  };
+  mkRubyAttrSet = dir: list:
+    let
+      mkName = r: "${dir}/ruby-${r.version}";
+      mkSetsList = map (r: nameValuePair (mkName r) r) list;
+    in
+      listToAttrs (mkSetsList);
 
-  mkRubyFile = dir: r: {
-    home.file."${mkRubyLoc dir r}" = mkRubySrc dir r;
-  };
-
-  mkRubies = dir: list: map (r: mkRubyFile dir r) list;
-
-  defaultRubies = with pkgs; [ ruby_2_7 ruby_3_0 ruby_3_1 ];
+  defaultRubies = lib.filterAttrs (k: _: lib.strings.hasPrefix "ruby_" k) pkgs;
 
 in
 {
@@ -26,20 +21,42 @@ in
     programs.ruby = {
       enable = mkEnableOption "A pseudo ruby-install module.";
 
-      /* fixme: this causes an infinite recursion when referenced! */
-      list = mkOption {
+      packages = mkOption {
         type = types.listOf types.package;
-        default = [];
-        description = "The set of ruby packages to appear in the user environment.";
+        default = builtins.attrValues defaultRubies;
+        defaultText = "[ ${lib.concatStringsSep " " (builtins.map (r: "pkgs.${r}") (builtins.attrNames defaultRubies)) } ]";
+        example = "[ pkgs.ruby_3_0 pkgs.ruby_3_1 ]";
+        description = ''
+          The set of ruby packages to appear in the user environment.
+
+          To discover what versions of ruby are available in your current version of nixpkgs:
+
+          ```
+          nix-instantiate --eval -E 'with import <nixpkgs> {}; __filter (lib.hasPrefix "ruby_") (__attrNames pkgs)'
+          // or
+          nix eval --impure --expr 'with import <nixpkgs> {}; __filter (lib.hasPrefix "ruby_") (__attrNames pkgs)'
+          ````
+
+          Helpful references for older ruby versions:
+          - https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=ruby
+          - https://til.codes/using-custom-versions-of-libraries-and-packages-using-nix/
+        '';
       };
 
       path = mkOption {
         type = types.str;
         default = ".rubies";
+        defaultText = ".rubies";
+        example = ".rbenv/versions";
         description = "The user directory to install the rubies into.";
       };
     };
   };
 
-  config = mkIf cfg.enable (mkMerge (mkRubies cfg.path defaultRubies));
+  config = mkIf cfg.enable {
+    home.file = mapAttrs (name: r: {
+      source = r;
+      target = name;
+    }) (mkRubyAttrSet cfg.path cfg.packages);
+  };
 }
