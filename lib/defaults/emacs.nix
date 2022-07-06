f4325309515cbb105e549b11d2306869a5a2a99b
Lachlan Deck <lachlan.deck@gmail.com>
2022-07-06 14:49:20 +1000

    ux: improve company mode highlighting for paganini theme

--- lib/defaults/emacs.nix
+++ lib/defaults/emacs.nix
@@ -1595,6 +1595,16 @@ in
           (setq lsp-completion-provider :capf)
 
           (global-company-mode)
+
+          ;; https://www.emacswiki.org/emacs/CompanyMode
+          (require 'color)
+          (let ((bg (face-attribute 'default :background)))
+            (custom-set-faces
+              `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+              `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+              `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+              `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+              `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
         '';
       };
 
