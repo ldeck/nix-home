a7d1bcbb1e01f22d67477ea23a1a874d5f4d6df6
Lachlan Deck <lachlan.deck@gmail.com>
2022-11-08 08:21:35 +1100

    macOS: rename freeruler to free-ruler and update
    based off brew cask free-ruler

---
+++ lib/defaults/apps/macOS/free-ruler.nix
@@ -0,0 +1,71 @@
+{ config, lib, pkgs, ... }:
+with lib;
+
+let
+  cfg = config.macOS.apps.free-ruler;
+  stdenv = pkgs.stdenv;
+  arch = if stdenv.isDarwin then stdenv.hostPlatform.darwinArch else stdenv.system;
+  toHyphenedLower = str:
+    (lib.strings.toLower (builtins.replaceStrings [" "] ["-"] str));
+
+  archSpecs = {
+    x86_64-darwin = {
+      version = "2.0.5";
+      revision = "";
+      arch = "amd64";
+      sha256 = "6332b9252a4fc58dbf4a5f74b5484d6ae20c2f4cb7db7a2c86020454fa66444d";
+    };
+    aarch64-darwin = {
+      version = "2.0.5";
+      revision = "";
+      arch = "arm64";
+      sha256 = "6332b9252a4fc58dbf4a5f74b5484d6ae20c2f4cb7db7a2c86020454fa66444d";
+    };
+  };
+
+in {
+  options = {
+    macOS.apps.free-ruler = {
+      enable = mkOption {
+        default = false;
+        description = "Whether to enable this app.";
+      };
+      sourceRoot = mkOption {
+        default = "Free Ruler.app";
+        description = "The app folder name to recursively copy from the install archive. e.g., Foo.app";
+      };
+      version = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.version;
+        description = "The version of the app.";
+      };
+      date = mkOption {
+        default = "";
+        description = "The build date (if applicable).";
+      };
+      revision = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.revision;
+        description = "The build number of the app (if applicable).";
+      };
+      sha256 = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.sha256;
+        description = "The sha256 for the app.";
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    home.packages =
+      (pkgs.callPackage ./lib/app.nix rec {
+        name = "Free-ruler";
+        description = "Horizontal and vertical rulers";
+        sourceRoot = cfg.sourceRoot;
+        version = cfg.version;
+        src = pkgs.fetchurl {
+          url = "https://github.com/pascalpp/FreeRuler/releases/download/v${cfg.version}/free-ruler-${cfg.version}.zip";
+          sha256 = cfg.sha256;
+          name = "${(toHyphenedLower name)}-${arch}-${version}.zip";
+        };
+        appcast = "https://formulae.brew.sh/api/cask/free-ruler.json";
+        homepage = "http://www.pascal.com/software/freeruler/";
+      });
+  };
+}
