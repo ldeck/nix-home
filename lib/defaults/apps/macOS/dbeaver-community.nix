1f333424edea749a0a979115ecb1718cb6e63b76
Lachlan Deck <lachlan.deck@gmail.com>
2023-09-19 14:28:04 +1000

    macos: add macos.app dbeaver-community

---
+++ lib/defaults/apps/macOS/dbeaver-community.nix
@@ -0,0 +1,79 @@
+{ config, lib, pkgs, ... }:
+with lib;
+
+let
+  cfg = config.macOS.apps.dbeaver-community;
+  stdenv = pkgs.stdenv;
+  arch = if stdenv.isDarwin then stdenv.hostPlatform.darwinArch else stdenv.system;
+  toHyphenedLower = str:
+    (lib.strings.toLower (builtins.replaceStrings [" "] ["-"] str));
+
+  archSpecs = {
+    x86_64-darwin = {
+      version = "23.2.0";
+      revision = "";
+      date = "";
+      arch = "amd64";
+      url = "https://dbeaver.io/files/${cfg.version}/dbeaver-ce-${cfg.version}-macos-x86_64.dmg";
+      sha256 = "19706c94bbb8752f2127d4f3a1b3358000fd0ed449a9f13dc75772dbb9ca30e7";
+    };
+    aarch64-darwin = {
+      version = "23.2.0";
+      revision = "";
+      date = "";
+      arch = "arm64";
+      url = "https://dbeaver.io/files/${cfg.version}/dbeaver-ce-${cfg.version}-macos-aarch64.dmg";
+      sha256 = "cc22ebe124974ad993815a5cd5b918be660ee3c0dd84b1f8b839a5e11491f1f9";
+    };
+  };
+
+in {
+  options = {
+    macOS.apps.dbeaver-community = {
+      enable = mkOption {
+        default = false;
+        description = "Whether to enable this app.";
+      };
+      sourceRoot = mkOption {
+        default = "DBeaver.app";
+        description = "The app folder name to recursively copy from the install archive. e.g., Foo.app";
+      };
+      version = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.version;
+        description = "The version of the app.";
+      };
+      date = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.date;
+        description = "The build date (if applicable).";
+      };
+      revision = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.revision;
+        description = "The build number of the app (if applicable).";
+      };
+      url = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.url;
+        description = "The url or url template for the archive.";
+      };
+      sha256 = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.sha256;
+        description = "The sha256 for the app.";
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    home.packages =
+      (pkgs.callPackage ./lib/app.nix rec {
+        name = "DBeaver";
+        description = "Universal database tool and SQL client";
+        sourceRoot = cfg.sourceRoot;
+        version = cfg.version;
+        src = pkgs.fetchurl {
+          url = cfg.url;
+          sha256 = cfg.sha256;
+          name = "${(toHyphenedLower name)}-${arch}-${version}.dmg";
+        };
+        appcast = "https://formulae.brew.sh/api/cask/dbeaver-community.json";
+        homepage = "https://dbeaver.io/";
+      });
+  };
+}
