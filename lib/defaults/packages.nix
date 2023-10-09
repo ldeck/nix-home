4d0b9f17b210891b925a6aa3de57c0addd161722
Lachlan Deck <lachlan.deck@gmail.com>
2023-10-09 11:51:33 +1100

    google sdk: add component gke gcloud auth plugin

--- lib/defaults/packages.nix
+++ lib/defaults/packages.nix
@@ -27,7 +27,7 @@
       ffmpeg
       gdb
       gnupg
-      google-cloud-sdk
+      (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
       httpie
       iconv
       jq
