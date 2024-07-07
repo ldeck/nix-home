cc57a6aaf1394a1880d782fd12dfcf3726c6850d
Lachlan Deck <lachlan.deck@gmail.com>
2024-07-07 14:37:12 +1000

    add macOS vnc-viewer 7.12.0

---
+++ lib/defaults/apps/macOS/vnc-viewer.nix
@@ -0,0 +1,85 @@
+{ config, lib, pkgs, ... }:
+with lib;
+
+let
+  cfg = config.macOS.apps.vnc-viewer;
+  stdenv = pkgs.stdenv;
+  arch = if stdenv.isDarwin then stdenv.hostPlatform.darwinArch else stdenv.system;
+  toHyphenedLower = str:
+    (lib.strings.toLower (builtins.replaceStrings [" "] ["-"] str));
+
+  archSpecs = {
+    x86_64-darwin = {
+      version = "7.12.0";
+      revision = "";
+      date = "";
+      arch = "amd64";
+      url = "https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-${cfg.version}-MacOSX-universal.dmg";
+      sha256 = "85a0f642c045f43a6cd55ff6b649002c073bfa4518dcf48d8fb0634c8aa735c5";
+      imagetype = "dmg";
+    };
+    aarch64-darwin = {
+      version = "7.12.0";
+      revision = "";
+      date = "";
+      arch = "arm64";
+      url = "https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-${cfg.version}-MacOSX-universal.dmg";
+      sha256 = "85a0f642c045f43a6cd55ff6b649002c073bfa4518dcf48d8fb0634c8aa735c5";
+      imagetype = "dmg";
+    };
+  };
+
+in {
+  options = {
+    macOS.apps.vnc-viewer = {
+      enable = mkOption {
+        default = false;
+        description = "Whether to enable this app.";
+      };
+      sourceRoot = mkOption {
+        default = "VNC Viewer.app";
+        description = "The app folder name to recursively copy from the install archive. e.g., Foo.app";
+      };
+      version = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.version;
+        description = "The version of the app.";
+      };
+      date = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.date;
+        description = "The build date (if applicable).";
+      };
+      revision = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.revision;
+        description = "The build number of the app (if applicable).";
+      };
+      url = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.url;
+        description = "The url or url template for the archive.";
+      };
+      sha256 = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.sha256;
+        description = "The sha256 for the app.";
+      };
+      imagetype = mkOption {
+        default = archSpecs.${stdenv.hostPlatform.system}.imagetype;
+        description = "The image type being downloaded.";
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    home.packages =
+      (pkgs.callPackage ./lib/app.nix rec {
+        name = "VNC Viewer";
+        description = "Remote desktop application focusing on security";
+        sourceRoot = cfg.sourceRoot;
+        version = cfg.version;
+        src = pkgs.fetchurl {
+          url = cfg.url;
+          sha256 = cfg.sha256;
+          name = "${(toHyphenedLower name)}-${arch}-${version}.${cfg.imagetype}";
+        };
+        appcast = "https://formulae.brew.sh/api/cask/vnc-viewer.json";
+        homepage = "https://www.realvnc.com/";
+      });
+  };
+}
