542617b97c1fbec2ea0f4e4571f36ec90a7d758e
Lachlan Deck <lachlan.deck@gmail.com>
2024-09-26 10:00:01 +1000

    update firefox

--- lib/defaults/apps/macOS/firefox.nix
+++ lib/defaults/apps/macOS/firefox.nix
@@ -10,21 +10,21 @@ let
 
   archSpecs = {
     x86_64-darwin = {
-      version = "127.0.2";
+      version = "130.0.1";
       revision = "";
       date = "";
       arch = "amd64";
       url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/${cfg.version}/mac/en-US/Firefox%20${cfg.version}.dmg";
-      sha256 = "af516f0222361a311e83de8ca9a27a99653b2c0a00a6653e25ab59046a44d128";
+      sha256 = "63ed878485d5498c269d95ba7e64f1104ed085b8e330b0ef0a565f85cc603426";
       imagetype = "dmg";
     };
     aarch64-darwin = {
-      version = "127.0.2";
+      version = "130.0.1";
       revision = "";
       date = "";
       arch = "arm64";
       url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/${cfg.version}/mac/en-US/Firefox%20${cfg.version}.dmg";
-      sha256 = "af516f0222361a311e83de8ca9a27a99653b2c0a00a6653e25ab59046a44d128";
+      sha256 = "63ed878485d5498c269d95ba7e64f1104ed085b8e330b0ef0a565f85cc603426";
       imagetype = "dmg";
     };
   };
