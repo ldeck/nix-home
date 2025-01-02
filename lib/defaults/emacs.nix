9bb27d6058c4329dfe9f8e33c2b0fcb5cfbf6fa2
Lachlan Deck <lachlan.deck@gmail.com>
2025-01-02 20:18:56 +1100

    emacs lsp lombok wip

--- lib/defaults/emacs.nix
+++ lib/defaults/emacs.nix
@@ -1257,11 +1257,62 @@ in
       lsp-java = {
         enable = true;
         defer = true;
-        hook = [ "(java-mode . rah-lsp)" ];
+        hook = [
+          "(java-mode . rah-lsp)"
+        ];
         bindLocal = {
           java-mode-map = { "C-c r o" = "lsp-java-organize-imports"; };
         };
-        config = ''
+        init = ''
+          (defvar lsp-java-lombok/enabled nil
+            "Indicates the LSP server should be started with Lombok.")
+
+          (defvar lsp-java-lombok/version nil
+            "When non-nil, use the specified Lombok version, otherwise use the latest.")
+
+          (defvar lsp-java-lombok/jar-url-base "https://projectlombok.org/downloads/"
+            "The base path to download Lombok jars from.")
+
+          (defvar lsp-java-lombok/dir user-emacs-directory
+            "The path on disk where lombok jars are saved.")
+
+          (defun lsp-java-lombok/jar-file ()
+            "Get the filename for the Lombok jar."
+            (concat "lombok"
+              (when lsp-java-lombok/version "-")
+                    lsp-java-lombok/version
+                    ".jar"))
+
+          (defun lsp-java-lombok/jar-path ()
+            "Generate the path on disk for the Lombok jar."
+            (concat user-emacs-directory (lsp-java-lombok/jar-file)))
+
+          (defun lsp-java-lombok/append-vmargs ()
+            "Apply lombok args to lsp-java-vmargs."
+            (setq lsp-java-vmargs
+              (append lsp-java-vmargs
+                (concat "-javaagent:" (lsp-java-lombok/jar-path)))))
+
+          (defun lsp-java-lombok/download-jar ()
+            "Download the latest lombok jar for use with LSP."
+            (let* ((lombok-url (url-generic-parse-url lsp-java-lombok/jar-url-base))
+                   (base-path (file-name-as-directory (url-filename lombok-url)))
+                   (file-path (concat base-path (lsp-java-lombok/jar-file))))
+               (setf (url-filename lombok-url) file-path)
+               (url-copy-file lombok-url (lsp-java-lombok/jar-path))))
+
+          (defun lsp-java-lombok/setup ()
+            "Download Lombok if it hasn't been downloaded already."
+            (when (not (file-exists-p (lsp-java-lombok/jar-path)))
+              (message "Could not find lombok for lsp-java.  Downloading...")
+              (lsp-java-lombok/download-jar)))
+
+          (defun lsp-java-lombok/init ()
+            "Initialize lsp-java-lombok."
+            (when lsp-java-lombok/enabled
+              (lsp-java-lombok/setup)
+              (lsp-java-lombok/append-vmargs)))
+
           (setq lsp-java-save-actions-organize-imports nil
                 lsp-java-completion-favorite-static-members
                     ["org.assertj.core.api.Assertions.*"
