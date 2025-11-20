0e50da8f14dbde9514d420f8ebf1d6b40e966f06
Lachlan Deck <lachlan.deck@gmail.com>
2025-11-20 16:37:02 +1100

    ensure direnvrc sources direnv/lib/*.sh

--- lib/defaults/direnv-defaults.nix
+++ lib/defaults/direnv-defaults.nix
@@ -2,9 +2,18 @@
 with lib;
 {
   programs.direnv.enable = mkOverride 100 true;
+  programs.direnv.enableBashIntegration = mkOverride 100 true;
+  programs.direnv.enableZshIntegration = mkOverride 100 true;
   programs.direnv.use_java.enable = mkOverride 100 true;
   programs.direnv.nix-direnv.enable = mkOverride 100 true;
   programs.direnv.stdlib = mkBefore ''
+    LIB=$HOME/.config/direnv/lib
+    if [ -d "$LIB" ]; then
+      for f in "$LIB/*.sh"; do
+        [ -f "$f" ] && source "$f"
+      done
+    fi
+
     if [ -f "$HOME/.nix-profile/share/nix-direnv/direnvrc" ]; then
        source "$HOME/.nix-profile/share/nix-direnv/direnvrc"
     fi
